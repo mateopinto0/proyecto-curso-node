e987c9a44c02627c06be29df57a04d659579c38f
mateopinto0 <pintomateo74@gmail.com>
2026-09-22 17:54:30 -0300

    agregado de post

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const URL_API=  'https://fakestoreapi.com/products';
 
-const method=process.argv[2];
+const method=process.argv[2].toUpperCase();
 const path=process.argv[3] || "";
+const title= process.argv[4];
+const price= process.argv[5];
+const category= process.argv[6];
 const[resource,id]=path.split("/");
 
 async function getProducts(){
@@ -18,6 +21,11 @@ async function getProducts(){
 
 async function deleteProduct() {
         
+    if(!id){
+        console.log("No existe el id del producto que desea eliminar")
+        return
+    }
+
     try{
             const url = id ? `${URL_API}/${id}` : `${URL_API}`;
             const res=await fetch(url, {
@@ -30,18 +38,36 @@ async function deleteProduct() {
     }
 }
 
+async function postProduct() {
+      const nuevoProducto = { title: title, price: parseFloat(price), category:category}
+
+    try{
+     const res= await fetch(URL_API,{
+        method: 'POST',
+        headers:{ 'Content-Type' : 'application/json'},
+        body: JSON.stringify(nuevoProducto)
+      });
+      const data= await res.json();
+      console.log(data)
+    }catch(error){
+        console.log(error)
+    }
+}
+if(resource != "products"){
+    console.log("Recurso no valido, utilice products")
+}else{
 switch(method){
     case 'GET':
-        if(resource == 'products'){
             getProducts();
-        }
         break;
     case 'DELETE':
-        if(resource == 'products'){
             deleteProduct();
-        }
+        break; 
+    case 'POST':
+            postProduct()
         break;    
     default:
         console.log("Comando no reconocido")
         break;    
+}
 }
